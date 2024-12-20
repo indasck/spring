cafbcdfa18949cc497a46b2ac023162ab39eab37
indasck <164610159+indasck@users.noreply.github.com>
2024-12-20 12:55:22 +0530

    Update practice.java

--- practice.java
+++ practice.java
@@ -1,6 +1,7 @@
 class practice
 {
 public static void main(String[] args){
-	System.out.println("this git reposit file");
+	int num=1, num=2;
+	System.out.println("this git reposit file..."num1+num2);
 }
 }
